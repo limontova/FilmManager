84a4ddcd43bb00e627e41c868a3101a56b205056
limontova <62797799+limontova@users.noreply.github.com>
2020-10-16 19:17:03 +0300

    Create class AboutFilm (fields and constructors)

---
+++ FilmManager/AboutFilm.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmManager
+{
+    public class AboutFilm
+    {
+        public AboutFilm(string title, string director, IEnumerable<string> actors) : this(title, director)
+        {
+            this.Actors = actors;
+        }
+        public AboutFilm(string title, string director) : this(title)
+        {
+            this.Director = director;
+        }
+        public AboutFilm(IEnumerable<string> actors)
+        {
+            this.Actors = actors;
+        }
+        public AboutFilm(string title)
+        {
+            this.Title = title;
+        }
+        public AboutFilm()
+        {
+            Title = new string("");
+            Actors = new List<string>();
+            Director = new string("");
+        }
+        public string Title { set; get; }
+        public IEnumerable<string> Actors { set; get; }
+        public string Director { set; get; }
+    }
+}
