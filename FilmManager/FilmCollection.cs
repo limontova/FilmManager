0de2ab0ab094ac358c9968555ab75b00f954a68c
limontova <62797799+limontova@users.noreply.github.com>
2020-11-07 18:40:58 +0300

    Add FilmCollection

---
+++ FilmManager/FilmCollection.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmManager
+{
+    public class FilmCollection
+    {
+        public List<FilmInfo> Films { set; get; } 
+
+        public List<FilmInfo> SearchByKeyword(string keyword)
+        {
+            List<FilmInfo> films = new List<FilmInfo>();
+            foreach (FilmInfo film in Films)
+            {
+                foreach (string keywordInFilm in film.Keywords)
+                {
+                    if(keywordInFilm == keyword)
+                    {
+                        films.Add(film);
+                        break;
+                    }
+                }
+            }
+            return films;
+        }
+
+        public List<FilmInfo> SearchByDirector(string director)
+        {
+            List<FilmInfo> films = new List<FilmInfo>();
+            foreach (FilmInfo film in Films)
+            {
+               if(film.Director == director)
+               {
+                    films.Add(film);
+                    break;
+               }
+            }
+            return films;
+        }
+
+        public List<FilmInfo> SearchByActor(string actor)
+        {
+            List<FilmInfo> films = new List<FilmInfo>();
+            foreach (FilmInfo film in Films)
+            {
+                foreach (string actorInFilm in film.Actors)
+                {
+                    if (actorInFilm == actor)
+                    {
+                        films.Add(film);
+                        break;
+                    }
+                }
+            }
+            return films;
+        }
+
+        public void AddFilm(FilmInfo filmToAdd)
+        {
+            Films.Add(filmToAdd);
+        }
+
+        private List<FilmInfo> QuickSort(List<FilmInfo> listToSort)
+        {
+            if (listToSort.Count <= 1)
+                return listToSort;
+            int pivotIndex = listToSort.Count / 2;
+            FilmInfo pivot = listToSort[pivotIndex];
+            List<FilmInfo> left = new List<FilmInfo>();
+            List<FilmInfo> right = new List<FilmInfo>();
+
+            for (int i = 0; i < listToSort.Count; i++)
+            {
+                if (i == pivotIndex) continue;
+
+                if (listToSort[i].Rating <= pivot.Rating)
+                {
+                    left.Add(listToSort[i]);
+                }
+                else
+                {
+                    right.Add(listToSort[i]);
+                }
+            }
+
+            List<FilmInfo> sorted = QuickSort(left);
+            sorted.Add(pivot);
+            sorted.AddRange(QuickSort(right));
+            return sorted;
+        }
+
+        public List<FilmInfo> GetFilmsSortedByRating()
+        {
+            return QuickSort(Films);
+        }   
+    }
+}
--- FilmManager/FilmInfo.cs
+++ FilmManager/FilmInfo.cs
@@ -5,32 +5,40 @@ using System.Threading.Tasks;
 
 namespace FilmManager
 {
-    public class AboutFilm
+    public class FilmInfo
     {
-        public AboutFilm(string title, string director, IEnumerable<string> actors) : this(title, director)
+        public FilmInfo(string title, string director, IEnumerable<string> actors) : this(title, director)
         {
             this.Actors = actors;
         }
-        public AboutFilm(string title, string director) : this(title)
+        public FilmInfo(string title, string director) : this(title)
         {
             this.Director = director;
         }
-        public AboutFilm(IEnumerable<string> actors)
+        public FilmInfo(IEnumerable<string> actors)
         {
             this.Actors = actors;
         }
-        public AboutFilm(string title)
+        public FilmInfo(string title)
         {
             this.Title = title;
+            Actors = new List<string>();
+            Director = new string("");
+
         }
-        public AboutFilm()
+        public FilmInfo()
         {
             Title = new string("");
             Actors = new List<string>();
             Director = new string("");
+            Keywords = new List<string>();
         }
         public string Title { set; get; }
         public IEnumerable<string> Actors { set; get; }
         public string Director { set; get; }
+        public IEnumerable<string> Keywords { set; get; }
+        public float Rating { set; get; }
+        public bool Latest { set; get; }
+        public float Appraisal { set; get; }
     }
 }
